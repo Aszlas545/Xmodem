69095b260e2e5aefc48ed5c5c12991309907352f
Andrzej Szlaski <242545@edu.p.lodz.pl>
2023-05-06 12:25:28 +0200

    upadte

--- Xmodem/Utils.cs
+++ Xmodem/Utils.cs
@@ -11,15 +11,14 @@ namespace GUI
 {
     internal class Utils
     {
-        const byte SOH = 0x01;
-        const byte EOT = 0x04;
-        const byte ACK = 0x06;
-        const byte NAK = 0x15;
-        const byte CAN = 0x18;
-        const byte C = 0x43;
-        const byte INC = 0x35;
+        public const byte SOH = 0x01;
+        public const byte EOT = 0x04;
+        public const byte ACK = 0x06;
+        public const byte NAK = 0x15;
+        public const byte CAN = 0x18;
+        public const byte C = 0x43;
+        public const byte INC = 0x35;
 
-        
         public string checkMessage(byte b)
         {
             switch (b)
@@ -36,7 +35,22 @@ namespace GUI
                     {
                         return "ACK";
                     }
-                    
+                case NAK:
+                    {
+                        return "NAK";
+                    }
+                case CAN:
+                    {
+                        return "CAN";
+                    }
+                case C:
+                    {
+                        return "C";
+                    }
+                case INC:
+                    {
+                        return "INC";
+                    }
             }
             return "nie ma przypisanego znaku";
         }
